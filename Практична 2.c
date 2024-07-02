89996086ba57e88c71ebaf09232a7930a61ad8db
eee1eli <166856755+eee1eli@users.noreply.github.com>
2024-07-02 10:59:36 +0300

    Add files via upload

---
+++ Практична 2.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+
+int ab(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+
+int qb(int a, int b) {
+    return (a * b) / ab(a, b);
+}
+
+int main() {
+    int p;
+    printf("Enter count of numbers: ");
+    scanf("%d", &p);
+
+    if (p < 2 || p > 20) {
+        printf("Count of numbers must be between 2 and 20\n");
+        return 1;
+    }
+
+    int numbers[20];
+    printf("Enter numbers: ");
+    for (int i = 0; i < p; i++) {
+        scanf("%d", &numbers[i]);
+    }
+
+    int result = numbers[0];
+    for (int i = 1; i < p; i++) {
+        result = qb(result, numbers[i]);
+    }
+
+    printf("NSD: %d\n", result);
+    return 0;
+}
